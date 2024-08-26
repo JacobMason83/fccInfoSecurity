1dd9e69142c955a58e71b9faa677773f5ac60de0
NeoZane1 <jmason062083@gmail.com>
2024-08-25 20:03:34 -0400

    adding changes to the project

--- myApp.js
+++ myApp.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
-
-
-
-
-
+const helmet = require('helmet');
+const timeInSeconds = 90*24*60*60;
+const bcrypt = require("bcrypt")
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action:'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen()); // used for ie browers that auto open html
+app.use(helmet.hsts({maxAge: timeInSeconds, force:true,}))
+app.use(helmet.dnsPrefetchControl()); // this disables DNS prefetching, at the cost of a performance penality
+app.use(helmet.noCache()); // disables the cache on the browser side, so that everyone has the latest release
+app.use(helmet.contentSecurityPolicy({
+  directives:{
+  defaultSrc:["'self'"],
+  scriptSrc:["'self'","trusted-cdn.com"]
+  }
+}))// allows us to set specific changes all things 
 
 
 
